f6d06009de1f8fa56dadcfc441755f32724280a1
Francisco Lacerda <francisco.pedro.lacerd@gmail.com>
2022-05-12 10:35:59 +0100

    Update flacerda.helpers.dates.js
    ups, no this on arrows functions. this points to windows. tuff luck.

--- flacerda.helpers.dates.js
+++ flacerda.helpers.dates.js
@@ -7,43 +7,45 @@ if (typeof flacerda.helpers === "undefined") {
 }
 
 flacerda.helpers.dates = {
-    vars: {
+vars: {
         months: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'],
         days: ['Domingo', 'Segunda-Feira', 'Terça-Feira', 'Quarta-Feira', 'Quinta-Feira', 'Sexta-Feira', 'Sábado']
     },
-    getDay: (val) => (new Date(this.parseDate(val))).getDate(),
-    getDayAndHour: (val) => {
-        let date = new Date(this.parseDate(val));
+    getDay: function (val) {
+        return (new Date(this.parseDate(val))).getDate();
+    },
+    getDayAndHour: function (val) {
+        var date = new Date(this.parseDate(val));
         return date.getDate() + ' '
             + this.vars.days[date.getDay()] + ' | '
             + this.addZero(date.getHours()) + ':'
             + this.addZero(date.getMinutes());
     },
-    getDayAndHourForCard: (val) => {
-        let date = new Date(this.parseDate(val));
+    getDayAndHourForCard: function (val) {
+        var date = new Date(this.parseDate(val));
         return this.vars.days[date.getDay()] + ' | '
             + this.addZero(date.getHours()) + ':'
             + this.addZero(date.getMinutes());
     },
-    getMonthAndYear: (val) => {
-        let date = new Date(this.parseDate(val));
+    getMonthAndYear: function (val) {
+        var date = new Date(this.parseDate(val));
         return this.vars.months[date.getMonth()] + ' ' + date.getFullYear();
     },
-    getDateTimeForTable: (val) =>  {
-        let date = new Date(this.parseDate(val));
+    getDateTimeForTable: function (val) {
+        var date = new Date(this.parseDate(val));
         return this.vars.days[date.getDay()] + '  ' +
             this.addZero(date.getDate()) + '/' + this.addZero(date.getMonth() + 1) + '/' + date.getFullYear() + ' ' +
             this.addZero(date.getHours()) + ':' + this.addZero(date.getMinutes());
     },
-    parseDate: (val) =>  { return val.indexOf('/Date') > -1 ? parseInt(val.replace("/Date(", "").replace(")/", "")) : val; },
-    addZero: (i) => {
+    parseDate: function (val) { return val.indexOf('/Date') > -1 ? parseInt(val.replace("/Date(", "").replace(")/", "")) : val; },
+    addZero: function (i) {
         if (i < 10) {
             i = "0" + i;
         }
         return i;
     },
-    getPtDate: (val) => {
-        let date = new Date(this.parseDate(val));
+    getPtDate: function (val) {
+        var date = new Date(this.parseDate(val));
         return this.addZero(date.getDate()) + '/' + this.addZero(date.getMonth() + 1) + '/' + date.getFullYear();
     }
 }
