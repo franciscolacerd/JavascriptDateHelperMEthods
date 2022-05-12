930779542b10b07394876f63d598f41889b62462
Francisco Lacerda <francisco.pedro.lacerd@gmail.com>
2022-05-12 09:56:59 +0100

    Update flacerda.helpers.dates.js
    Changed methods to arrow functions. Corrected namespace. vars changed to let.

--- flacerda.helpers.dates.js
+++ flacerda.helpers.dates.js
@@ -1,9 +1,9 @@
 if (typeof flacerda === "undefined") {
-    var vasp = {};
+    var flacerda = {};
 }
 
 if (typeof flacerda.helpers === "undefined") {
-    vasp = { helpers: {} };
+    flacerda = { helpers: {} };
 }
 
 flacerda.helpers.dates = {
@@ -12,42 +12,39 @@ flacerda.helpers.dates = {
         months: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'],
         days: ['Domingo', 'Segunda-Feira', 'Terça-Feira', 'Quarta-Feira', 'Quinta-Feira', 'Sexta-Feira', 'Sábado']
     },
-    getDay: function (val) {
-        var currentEventDate = new Date(this.parseDate(val));
-        return currentEventDate.getDate();
+    getDay: (val) => (new Date(this.parseDate(val))).getDate(),
+    getDayAndHour: (val) => {
+        let date = new Date(this.parseDate(val));
+        return date.getDate() + ' '
+            + this.vars.days[date.getDay()] + ' | '
+            + this.addZero(date.getHours()) + ':'
+            + this.addZero(date.getMinutes());
     },
-    getDayAndHour: function (val) {
-        var currentEventDate = new Date(this.parseDate(val));
-        return currentEventDate.getDate() + ' '
-            + this.vars.days[currentEventDate.getDay()] + ' | '
-            + this.addZero(currentEventDate.getHours()) + ':'
-            + this.addZero(currentEventDate.getMinutes());
+    getDayAndHourForCard: (val) => {
+        let date = new Date(this.parseDate(val));
+        return this.vars.days[date.getDay()] + ' | '
+            + this.addZero(date.getHours()) + ':'
+            + this.addZero(date.getMinutes());
     },
-    getDayAndHourForCard: function (val) {
-        var currentEventDate = new Date(this.parseDate(val));
-        return this.vars.days[currentEventDate.getDay()] + ' | '
-            + this.addZero(currentEventDate.getHours()) + ':'
-            + this.addZero(currentEventDate.getMinutes());
+    getMonthAndYear: (val) => {
+        let date = new Date(this.parseDate(val));
+        return this.vars.months[date.getMonth()] + ' ' + date.getFullYear();
     },
-    getMonthAndYear: function (val) {
-        var currentEventDate = new Date(this.parseDate(val));
-        return this.vars.months[currentEventDate.getMonth()] + ' ' + currentEventDate.getFullYear();
+    getDateTimeForTable: (val) =>  {
+        let date = new Date(this.parseDate(val));
+        return this.vars.days[date.getDay()] + '  ' +
+            this.addZero(date.getDate()) + '/' + this.addZero(date.getMonth() + 1) + '/' + date.getFullYear() + ' ' +
+            this.addZero(date.getHours()) + ':' + this.addZero(date.getMinutes());
     },
-    getDateTimeForTable: function (val) {
-        var eventDate = new Date(this.parseDate(val));
-        return this.vars.days[eventDate.getDay()] + '  ' +
-            this.addZero(eventDate.getDate()) + '/' + this.addZero(eventDate.getMonth() + 1) + '/' + eventDate.getFullYear() + ' ' +
-            this.addZero(eventDate.getHours()) + ':' + this.addZero(eventDate.getMinutes());
-    },
-    parseDate: function (val) { return val.indexOf('/Date') > -1 ? parseInt(val.replace("/Date(", "").replace(")/", "")) : val; },
-    addZero: function (i) {
+    parseDate: (val) =>  { return val.indexOf('/Date') > -1 ? parseInt(val.replace("/Date(", "").replace(")/", "")) : val; },
+    addZero: (i) => {
         if (i < 10) {
             i = "0" + i;
         }
         return i;
     },
-    getPtDate: function (val) {
-        var date = new Date(this.parseDate(val));
+    getPtDate: (val) => {
+        let date = new Date(this.parseDate(val));
         return this.addZero(date.getDate()) + '/' + this.addZero(date.getMonth() + 1) + '/' + date.getFullYear();
     }
 }
